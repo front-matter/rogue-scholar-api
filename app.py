a72a8752e75d2cf9c3c96745ee53c7cd7b51ebce
Martin Fenner <martin@front-matter.io>
2023-10-03 13:19:28 +0200

    remove non-api path

--- app.py
+++ app.py
@@ -2,12 +2,6 @@ from quart import Quart, render_template, websocket
 
 app = Quart(__name__)
 
-
-@app.route("/")
-async def hello():
-    return await render_template("index.html")
-
-
 @app.route("/api")
 async def json():
     return {"hello": "world"}
