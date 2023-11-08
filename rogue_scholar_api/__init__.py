9bd463f089c4fd3b40051f37a25c7631bce92d0b
Martin Fenner <martin@front-matter.io>
2023-11-08 12:17:36 +0100

    remove sentry sampling

--- rogue_scholar_api/__init__.py
+++ rogue_scholar_api/__init__.py
@@ -43,9 +43,6 @@ version = "0.7.1"  # TODO: importlib.metadata.version('rogue-scholar-api')
 
 sentry_sdk.init(
     dsn=environ["QUART_SENTRY_DSN"],
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    traces_sample_rate=1.0,
 )
 app = Quart(__name__)
 app.config.from_prefixed_env()
