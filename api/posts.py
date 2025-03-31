8758e75b59f8e3afca564f5eb2eaabd1908a21c2
Martin Fenner <martin@front-matter.io>
2025-03-31 13:50:55 +0200

    Update posts.py

--- api/posts.py
+++ api/posts.py
@@ -1827,7 +1827,7 @@ def create_record(record, guid: str, community_id: str, category_id: str):
         headers = {"Authorization": f"Bearer {environ['QUART_INVENIORDM_TOKEN']}"}
         response = httpx.post(url, headers=headers, timeout=10.0)
         if response.status_code >= 400:
-            print(response.status_code, "publish_draft_record", guid)
+            print(response.status_code, "publish_draft_record", guid, response.json())
             return response.json()
 
         # add draft record to blog community
